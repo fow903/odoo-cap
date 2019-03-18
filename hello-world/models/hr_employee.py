1621940188c0d0b9c19305e54a81e425941bbd01
armp02 <edwin2delossantos@gmail.com>
2019-03-18 18:33:02 -0400

    one2many field

--- hello-world/models/hr_employee.py
+++ hello-world/models/hr_employee.py
@@ -1,6 +1,15 @@
 from odoo import fields, models, api
 
-class HrUniform(models.Model):
+class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    test = fields.Char(string="Size Zapato")
+    uniformes = fields.One2many('hr.uniform','employee_id',string="uniformes")
+
+
+class HrUniform(models.Model):
+    _name = 'hr.uniform'
+
+    employee_id = fields.Many2one(comodel_name='hr.employee')
+    test = fields.Char()
+
+
